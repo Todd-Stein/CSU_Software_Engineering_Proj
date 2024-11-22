8b0642b4eb5a349a20e5383f195115186e9120ea
Todd-Stein <77040193+Todd-Stein@users.noreply.github.com>
2024-11-21 21:35:28 -0500

    Add project files.

---
+++ LEDPanelDesktopClient/LEDPanelDesktopClient.cpp
@@ -0,0 +1,12 @@
+﻿// LEDPanelDesktopClient.cpp : Defines the entry point for the application.
+//
+
+#include "LEDPanelDesktopClient.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
