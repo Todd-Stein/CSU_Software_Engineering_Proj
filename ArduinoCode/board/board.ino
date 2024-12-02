b13162347971cdaca93478922cb3faab8edc62f6
Todd-Stein <77040193+Todd-Stein@users.noreply.github.com>
2024-12-01 19:09:38 -0500

    in a semi-working state

--- ArduinoCode/board/board.ino
+++ ArduinoCode/board/board.ino
@@ -1,99 +1,75 @@
 #include <FastLED.h>
-#define MAX_LED 1024
+#define LED_WIDE 20
+#define LED_HIGH 40
+#define NUM_LEDS LED_WIDE*LED_HIGH
 #define DATA_PIN 16
-enum class State {NotConnected, ConnectionEstablished, RecievingData, SendingData, ChangeLED, TEST_DoNothing, TEST_ReadSerial};
+enum class State {NotConnected, ConnectionEstablished, AwaitingData, RecievingData, SendingData};
 State currentState = State::ConnectionEstablished;
-unsigned int ledWide = 0;
-unsigned int ledHigh = 0;
-unsigned int totalLEDs = 0;
-CRGB leds[MAX_LED];
+//unsigned int ledWide = 0;
+//unsigned int ledHigh = 0;
+//unsigned int totalLEDs = 0;
+CRGB leds[NUM_LEDS];
 
 void setup() {
-  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, MAX_LED);
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
   // put your setup code here, to run once:
   Serial.begin(9600);
-  Serial1.begin(9600);
-  //currentState = State::TEST_ReadSerial;
+  currentState = State::ConnectionEstablished;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   switch(currentState) {
-    case State::NotConnected:
+    case (State::ConnectionEstablished):
     {
-      Serial.begin(9600);
-      break;
-    }
-    case State::ConnectionEstablished:
-    {
-        if (Serial.available()>0) {
-        char initialStream [5];
-        Serial.readBytes(initialStream, 5);
-        if(initialStream[0]=='S' && initialStream[1]== 'T' && (char)initialStream[2]=='A' && (char)initialStream[3]=='R' && (char)initialStream[4]=='T') {
-          Serial.println("Start data stream.");
-          currentState = State::RecievingData;
-          byte newDims [2];
-          Serial.readBytes(newDims, 2);
-          ledWide = (unsigned int)newDims[0];
-          ledHigh = (unsigned int)newDims[1];
-          Serial.println("New dimensions ");
-          Serial.print(ledWide);
-          Serial.print("\n");
-          Serial.print(ledHigh);
-          Serial.print("\n\n\n");
-          totalLEDs = ledWide*ledHigh;
-        }
-        //break;
-      }
-      Serial.println("No data");
-      break;
-    }
-    case State::ChangeLED:
-    { 
-      //delete [] leds;
-      //FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, totalLEDs);
-      currentState = State::RecievingData;
-      break;
-    }
-    case State::RecievingData:
-    {
-      byte colors [totalLEDs*3];
-      int ledIdx = 0;
-      Serial.readBytes(colors, totalLEDs*3);
-      for(int i = 0; i<totalLEDs*3; i+=3) {
-        delay(2000);
-        Serial.println("next color");
-        Serial.print(i);
-        Serial.print(totalLEDs);
-        CRGB color;
-        color.red = colors[i+2];
-        color.blue = colors[i];
-        color.green = colors[i+1];
-        leds[ledIdx] = color;
-        ledIdx++;
-      }
-      Serial.println("colors set");
-      currentState = State::SendingData;
-      break;
-    }
-    case State::SendingData:
-    {
-      Serial.write("finished recieving data");
-      currentState = State::ConnectionEstablished;
-      break;
-    }
-    case State::TEST_DoNothing:
-    {
-      delay(1000);
-      break;
-    }
-    case State::TEST_ReadSerial:
-      char data[100];
-      while (Serial.available()>0) {
-        Serial.readBytes(data, 20);
-        for(int i = 0; i<20; i++) {
-          Serial.println((char)data[i]);
+      if(Serial.available()>0) {
+        char get[3];
+        Serial.readBytes(get, 3);
+        if(get[0]=='G'&&get[1]=='E'&&get[2]=='T') {
+          //Serial.println("newDims");
+          char dimensions[4];
+          dimensions[0] = (char)LED_WIDE;
+          dimensions[1] = 'W';
+          dimensions[2] = (char)LED_HIGH;
+          dimensions[3] = 'H';
+          Serial.write(dimensions, 4);
+          //Serial.println("endNewDims");
+          currentState = State::AwaitingData;
         }
       }
+      break;
+    }
+    case (State::AwaitingData): {
+      //Serial.println("state2");
+      if(Serial.available()>0) {
+        currentState = State::RecievingData;
+      }
+      break;
+    }
+    case (State::RecievingData): {
+      //Serial.println("state 3");
+      byte dataPacket[(NUM_LEDS*3)+4];
+      Serial.readBytes(dataPacket, (NUM_LEDS*3)+4);
+      if(dataPacket[0]=='S' && dataPacket[1]=='T') {
+        int ledIdx = 0;
+        for(int i = 0; i<NUM_LEDS*3; i+=3) {
+          CRGB pixelColor;
+          pixelColor.red = dataPacket[i+2];
+          pixelColor.blue = dataPacket[i];
+          pixelColor.green = dataPacket[i+1];
+          leds[ledIdx] = pixelColor;
+          ledIdx++;
+        }
+        int lastIndex = (NUM_LEDS*3)+3;
+        if(dataPacket[lastIndex-1] == 'E' && dataPacket[lastIndex]=='N') {
+          currentState = State::AwaitingData;
+        }
+      }
+      break;
+    }
+  }
+  char reset[5];
+  Serial.readBytes(reset, 5);
+  if(reset[0]=='R'&&reset[1]=='E'&&reset[2]=='S'&&reset[3]=='E'&&reset[4]=='T') {
+    currentState = State::ConnectionEstablished;
   }
 }
--- LEDPanelDesktopClient/main.cpp
+++ LEDPanelDesktopClient/main.cpp
@@ -1,3 +1,6 @@
+#ifndef ASIO_STANDALONE
+#define ASIO_STANDALONE
+#endif
 #include <iostream>
 #include <string>
 #include <unordered_map>
@@ -36,6 +39,12 @@ cv::Mat originalImage;
 cv::Mat previewImage;
 cv::Mat sendImage;
 
+asio::io_context ioCTX;
+asio::serial_port* ser = nullptr;
+
+std::string portID = "COM7";
+unsigned int baudRate = 9600;
+
 struct Dimensions {
 	int w;
 	int h;
@@ -45,10 +54,15 @@ struct Dimensions {
 Dimensions outImageDimensions{ 0, 0 };
 Dimensions inImageDimensions{ 0, 0 };
 
+char tempPath [1000] = "File path.";
+char tempPortId [100] = "Serial port";
 
 bool sendData = false;
 bool isImageLoaded = false;
 bool updateScale = true;
+bool isPortOpen = false;
+bool canOpenPort = false;
+
 
 void InitSDL();
 void InitIMGUI();
@@ -56,9 +70,17 @@ void Render();
 void InitPixelFormats();
 void UpdatePrevImageScale(Dimensions);
 bool CheckPathValid(std::string);
+bool CheckPort(std::string);
 
 int main(int argc, char* argv[]) {
-	
+	////set text box values to all spaces
+	//for (int i = 0; i < 1000; i++) {
+	//	tempPath[i] = ' ';
+	//}
+	//for (int i = 0; i < 100; i++) {
+	//	tempPortId[i] = ' ';
+	//}
+
 	mainWindowWidth = (int*)malloc(sizeof(int));
 	mainWindowHeight = (int*)malloc(sizeof(int));
 
@@ -72,12 +94,13 @@ int main(int argc, char* argv[]) {
 	inImageDimensions.h = originalImage.cols;
 	outImageDimensions = { 20,20 };
 
+	bool isOpen;
+	
 	bool run = true;
 	SDL_Event evt;
 	while (run) {
 		while (SDL_PollEvent(&evt)) {
 			ImGui_ImplSDL3_ProcessEvent(&evt);
-			std::cout << "Event loop" << std::endl;
 			switch (evt.type) {
 				case SDL_EVENT_QUIT:
 					run = false;
@@ -89,6 +112,33 @@ int main(int argc, char* argv[]) {
 		}
 		if (isImageLoaded && updateScale)
 			UpdatePrevImageScale(outImageDimensions);
+		char InDims[20];
+		if (canOpenPort) {
+			asio::error_code ec;
+			if (ser != nullptr)
+				delete ser;
+			ser = new asio::serial_port(ioCTX, portID);
+
+			ser->set_option(asio::serial_port_base::baud_rate(9600));
+			ser->set_option(asio::serial_port_base::character_size(8));
+			ser->set_option(asio::serial_port_base::parity(asio::serial_port_base::parity::none));
+			ser->set_option(asio::serial_port_base::stop_bits(asio::serial_port_base::stop_bits::one));
+			ser->set_option(asio::serial_port_base::flow_control(asio::serial_port_base::flow_control::none));
+
+			//since reseting generates random bytes i want to clear that out before i send and recieve data
+			//char resetBuff[100];
+			//std::cout << "clearing reset buff" << std::endl;
+			//ser->read_some(asio::buffer(resetBuff), ec);
+
+			//get the dimentions
+			ser->write_some(asio::buffer("GET"), ec);
+
+			canOpenPort = false;
+			isPortOpen = true;
+			ser->read_some(asio::buffer(InDims), ec);
+			outImageDimensions.w = InDims[0];
+			outImageDimensions.h = InDims[0];
+		}
 		Render();
 		if (outImageDimensions.w <= 20) {
 			outImageDimensions.w = 20;
@@ -96,14 +146,20 @@ int main(int argc, char* argv[]) {
 		if (outImageDimensions.h <= 20) {
 			outImageDimensions.h = 20;
 		}
+		//asio::write(ser, asio::buffer(bufferTest));
+		//
+		//char inBuff[29];
+		//try {
+		//	ser.read_some(asio::buffer(inBuff));
+		//}
+		//catch (std::exception e) {
+		//	std::cout << "couldn't read buffer" << std::endl;
+		//}
+		//std::cout << inBuff << std::endl;
 	}
 	SDL_Quit();
 	return EXIT_SUCCESS;
 }
-void InitPixelFormats() {
-	cvToSDLPixelForm.insert(std::pair<size_t, size_t>(CV_8U, SDL_PIXELFORMAT_BGR24));
-}
-
 void InitSDL() {
 	if (!SDL_Init(SDL_INIT_VIDEO)) {
 		std::cout << "Failed to init SDL" << std::endl;
@@ -112,7 +168,7 @@ void InitSDL() {
 	mainWindow = SDL_CreateWindow("LED Desktop Client", 1600, 900, SDL_WINDOW_MAXIMIZED | SDL_WINDOW_RESIZABLE);
 	SDL_ShowWindow(mainWindow);
 	mainWinRender = SDL_CreateRenderer(mainWindow, NULL);
-	SDL_SetRenderDrawColor(mainWinRender, 255, 0, 0, 255);
+	SDL_SetRenderDrawColor(mainWinRender, 0, 0, 0, 255);
 	
 	std::cout << "Init Success" << std::endl;
 }
@@ -138,13 +194,23 @@ void Render() {
 	ImGui::Begin("Dock panel", & open, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_AlwaysAutoResize);
 	{
 		ImGui::Text("preview image goes here");
-		bool updateW = ImGui::InputInt("Out Width", &outImageDimensions.w);
+		std::string str = "Output Dimensions:\t";
+		str += std::to_string(outImageDimensions.w);
+		str += "x";
+		str += std::to_string(outImageDimensions.h);
+		ImGui::Text(str.c_str());
+		/*bool updateW = ImGui::InputInt("Out Width", &outImageDimensions.w);
 		bool updateH = ImGui::InputInt("Out Height", &outImageDimensions.h);
-		updateScale = (updateW || updateH);
+		updateScale = (updateW || updateH);*/
+		
+		ImGui::PushStyleVar(ImGuiStyleVar_Alpha, ImGui::GetStyle().Alpha * 0.5f);
+		ImGui::BeginDisabled(isImageLoaded && isPortOpen);
 		if (!sendData)
 			sendData = ImGui::Button("Begin Send Data");
 		else
 			sendData = !ImGui::Button("End Send Data");
+		ImGui::EndDisabled();
+		ImGui::PopStyleVar();
 		ImGui::Image(imgPrevIMGUI, ImVec2(200, 200));
 		
 	}
@@ -153,15 +219,25 @@ void Render() {
 	ImGui::SetNextWindowSize(ImVec2(*mainWindowWidth / 2, *mainWindowHeight));
 	ImGui::Begin("Dock panel 2", & open, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_AlwaysAutoResize);
 	{
+		if (sendData)
+			ImGui::PushStyleVar(ImGuiStyleVar_Alpha, ImGui::GetStyle().Alpha * 0.5f);
+		ImGui::BeginDisabled(sendData);
 		ImGui::Text("file selector goes here");
-		char* tempPath = (char*)malloc(sizeof(char)*1000);
 		if (ImGui::InputText("Type file directory.", tempPath, 1000)) {
 			std::cout << tempPath << std::endl;
 			filePath = std::string(tempPath);
 			isImageLoaded = CheckPathValid(filePath);
 			updateScale = isImageLoaded;
 		}
-		free(tempPath);
+		if (ImGui::InputText("Type serial port ID here.", tempPortId, 100)) {
+			std::cout << "Check port" << std::endl;
+			portID = tempPortId;
+			canOpenPort = CheckPort(portID);
+		}
+		
+			ImGui::EndDisabled();
+			if (sendData)
+				ImGui::PopStyleVar();
 	}
 	ImGui::End();
 	ImGui::Render();
@@ -196,4 +272,19 @@ bool CheckPathValid(std::string path) {
 		return false;
 	originalImage = cv::imread(path);
 	return true;
+}
+bool CheckPort(std::string port) {
+	try {
+		if (ser != nullptr)
+			ser->close();
+		asio::serial_port testPort(ioCTX, port);
+		testPort.close();
+		return true;
+	}
+	catch (std::exception e) {
+		std::cout << "Cannot open " << port << std::endl;
+		isPortOpen = false;
+		return false;
+	}
+	return false;
 }
